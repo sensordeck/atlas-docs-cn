3ec8d7a1848beecf8fa4dba45b20ee9dc8729664
jc691668-a11y <jc691668@gmail.com>
2026-03-19 18:27:07 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -5,15 +5,51 @@
  */
 const sidebars = {
   tutorialSidebar: [
-    'intro',
-    'software/sensor-synchronization',
-    'hardware/fusion-platform',
-    'software/DSIL-SDK',
-    'software/ros2-integration',
-    'software/evaluation-kit',
-    'evaluation/oem-pilot',
-    'evaluation/faq',
-    'evaluation/download',
+    {
+      type: 'doc',
+      id: 'intro',
+      label: 'Atlas 开发文档',
+    },
+    {
+      type: 'doc',
+      id: 'software/sensor-synchronization',
+      label: '传感器同步',
+    },
+    {
+      type: 'doc',
+      id: 'hardware/fusion-platform',
+      label: '硬件架构',
+    },
+    {
+      type: 'doc',
+      id: 'software/DSIL-SDK',
+      label: 'DSIL SDK',
+    },
+    {
+      type: 'doc',
+      id: 'software/ros2-integration',
+      label: 'ROS2 集成',
+    },
+    {
+      type: 'doc',
+      id: 'software/evaluation-kit',
+      label: 'Atlas 评估套件',
+    },
+    {
+      type: 'doc',
+      id: 'evaluation/oem-pilot',
+      label: 'Atlas 白标 OEM 集成试点计划',
+    },
+    {
+      type: 'doc',
+      id: 'evaluation/faq',
+      label: '集成问答 FAQ',
+    },
+    {
+      type: 'doc',
+      id: 'evaluation/download',
+      label: '下载中心',
+    },
   ],
 };
 
